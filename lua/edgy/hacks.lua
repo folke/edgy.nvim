eef534ccdf5663eead994c7d46dc3259cbd74cb3
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-06-01 21:49:33 +0200

    fix(hacks): disable splitkeep for now when resizing. See upstream PR

--- lua/edgy/hacks.lua
+++ lua/edgy/hacks.lua
@@ -5,6 +5,7 @@ function M.setup()
   ffi.cdef([[
     void win_setheight_win(int width, void *wp);
     void *find_window_by_handle(int window, void *err);
+    bool skip_win_fix_cursor;
   ]])
 
   vim.api.nvim_win_set_height = function(win, height)
@@ -15,7 +16,9 @@ function M.setup()
     if win_t == nil then
       return
     end
+    ffi.C.skip_win_fix_cursor = win ~= vim.api.nvim_get_current_win()
     ffi.C.win_setheight_win(height, win_t)
+    ffi.C.skip_win_fix_cursor = false
   end
 end
 
