e4ca3ab70275472b685ddaa91fd627fb44e01750
Folke Lemaitre <folke.lemaitre@gmail.com>
2023-06-03 15:24:03 +0200

    feat: added close methods

--- lua/edgy/init.lua
+++ lua/edgy/init.lua
@@ -5,4 +5,16 @@ function M.setup(opts)
   require("edgy.config").setup(opts)
 end
 
+---@param pos? Edgy.Pos
+function M.close(pos)
+  local Config = require("edgy.config")
+  for p, sidebar in pairs(Config.layout) do
+    if p == pos or pos == nil then
+      for _, win in ipairs(sidebar.wins) do
+        pcall(vim.api.nvim_win_close, win.win, true)
+      end
+    end
+  end
+end
+
 return M
